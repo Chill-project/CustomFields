1747ba5f0dbaf33002be534e696a7b35e217b80a
Marc Ducobu <marc@champs-libres.coop>
2014-11-08 15:19:30 +0100

    Adding Title for Custom Field

---
+++ CustomFields/CustomFieldTitle.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * Chill is a software for social workers 
+ *
+ * Copyright (C) 2014, Champs Libres Cooperative SCRLFS, <http://www.champs-libres.coop>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Chill\CustomFieldsBundle\CustomFields;
+
+use Chill\CustomFieldsBundle\CustomFields\CustomFieldInterface;
+use Chill\CustomFieldsBundle\Entity\CustomField;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class CustomFieldTitle implements CustomFieldInterface
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+ 
+    public function buildForm(FormBuilderInterface $builder, CustomField $customField)
+    {
+        $builder->add($customField->getSlug(), 'custom_field_title', array(
+            'label' => $customField->getLabel(
+                $this->requestStack->getCurrentRequest()->getLocale())
+        ));
+    }
+
+    public function render($value, CustomField $customField)
+    {
+       return $value; 
+    }
+
+    public function serialize($value, CustomField $customField)
+    {
+        return $value;
+    }
+
+    public function deserialize($serialized, CustomField $customField)
+    {
+        return $serialized;
+    }
+
+    public function getName()
+    {
+        return 'title';
+    }
+
+    public function buildOptionsForm(FormBuilderInterface $builder)
+    {
+      return $builder;
+    }
+}
